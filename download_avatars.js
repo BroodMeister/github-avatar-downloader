a37ec3dbd21420b66613d807949f4d54f981d22a
broodmeister <isaiahchrst80@gmail.com>
2017-10-09 21:50:01 +0000

    Did up to step 5.

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,33 @@
 var request = require('request');
+var fs = require('fs');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+var GITHUB_USER = 'BroodMeister';
+var GITHUB_TOKEN = '52c5547c7546cce465087137e1411b4650507993';
+
+function getRepoContributors(repoOwner, repoName, callback) {
+  var options = {
+    url: 'https://'
+      + GITHUB_USER + ':' + GITHUB_TOKEN
+      + '@api.github.com/repos/'
+      + repoOwner + '/' + repoName
+      + '/contributors',
+
+    headers: {
+      'User-Agent': 'request'
+    }
+  }
+  request.get(options)
+    .on('error', function(err) {
+      throw err;
+    })
+    .on('response', function(response) {
+      console.log('%s: %s', response.statusCode, response.statusMessage);
+    });
+}
+
+console.log('Welcome to Github Avatar Downloader!');
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
